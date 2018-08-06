91c5ba80238e1db6005d97af369bd58383239aea
Daniel Gary <daniel@gatewayapps.com>
2018-08-06 16:42:25 -0500

    added tests

---
+++ src/lib/LoaderOptions.spec.ts
@@ -0,0 +1,15 @@
+import LoaderOptions from './LoaderOptions'
+
+import {expect} from 'chai'
+import 'mocha'
+import InvalidConsoleError from '../Errors/InvalidConsoleError';
+
+describe('LoaderOptions constructor', ()=>{
+    it('Should create a new LoaderOptions instance when passed arguments and a console', ()=>{
+      const options = new LoaderOptions('', console)
+      expect(options).to.deep.equal({args: '', console: console})
+    })
+    it('Should throw an InvalidConsoleError when it receives a console that does not implement IConsole', ()=>{
+        expect( () => {const options = new LoaderOptions('', {})}).to.throw(InvalidConsoleError)
+    })
+})
--- src/lib/LoaderOptions.ts
+++ src/lib/LoaderOptions.ts
@@ -1,5 +1,5 @@
 import { IConsole, isConsole } from './IConsole'
-
+import InvalidConsoleError from '../Errors/InvalidConsoleError'
 
 export default class LoaderOptions {
     public args: string
@@ -7,7 +7,7 @@ export default class LoaderOptions {
 
     constructor(args: string, console: any) {
         if (!isConsole(console)) {
-            throw new Error(`LoaderOptions received a console argument that does not implement IConsole`)
+            throw new InvalidConsoleError(console)
         } else {
             this.args = args
             this.console = console
