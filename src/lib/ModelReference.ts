4ec3b1caab01df4b2dfab5ec9bdeeb2cdf85f8f5
Daniel Gary <daniel@gatewayapps.com>
2018-10-09 12:37:43 -0500

    added support for optional references

--- src/lib/ModelReference.ts
+++ src/lib/ModelReference.ts
@@ -10,9 +10,11 @@ export default class ModelReference {
     public RelationType: RelationTypes
     public LocalProperty?: string
     public ProxyModel?: string
+    public AllowNull: boolean
 
-    constructor(localProperty: string, foreignModel: string, relationType: RelationTypes, proxyModel?: string) {
+    constructor(localProperty: string, foreignModel: string, relationType: RelationTypes, allowNull: boolean, proxyModel?: string) {
         this.ForeignModel = foreignModel
+        this.AllowNull = allowNull
         this.RelationType = relationType
         this.LocalProperty = localProperty
         this.ProxyModel = proxyModel
