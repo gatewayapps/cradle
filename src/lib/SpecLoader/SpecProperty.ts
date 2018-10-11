bcc5bc45a3022c2b48e07c5fafcc4181b98bd948
Justin Neff <justin@gatewayapps.com>
2018-10-11 12:49:58 -0500

    Added additional tests for SpecPropertyTypeParser (#16)

--- src/lib/SpecLoader/SpecProperty.ts
+++ src/lib/SpecLoader/SpecProperty.ts
@@ -1,40 +1,60 @@
 import { IntegerAutogenerateOptions } from '../PropertyTypes/IntegerPropertyType'
 
+export interface ISpecPropertyOptions {
+  allowedValues?: any
+  autogenerateOptions?: any
+  defaultValue?: any
+  deleteFlag?: boolean
+  isArray?: boolean
+  length?: any
+  maxValue?: any
+  minValue?: any
+  nullable?: boolean
+  precision?: number
+  primaryKey?: boolean
+  scale?: number
+  unique?: boolean
+}
+
 export default class SpecProperty {
   public PropertyType: string
-  public AutogenerateOptions!: IntegerAutogenerateOptions | boolean
+  public AutogenerateOptions?: IntegerAutogenerateOptions | boolean
   public Nullable: boolean
-  public AllowedValues!: any[]
-  public DefaultValue!: any
-  public MinValue!: any
-  public MaxValue!: any
+  public AllowedValues?: any[]
+  public DefaultValue?: any
+  public MinValue?: any
+  public MaxValue?: any
   public PrimaryKey: boolean
   public Unique: boolean
   public DeleteFlag: boolean
-  public Length!: number
+  public Length?: number
   public IsArray: boolean
+  public Precision?: number
+  public Scale?: number
 
-  constructor(propertyType: string, nullable: boolean = false, primaryKey: boolean = false, unique: boolean = false, deleteFlag: boolean = false, autogenerateOptions?: any, allowedValues?: any, defaultValue?: any, minValue?: any, maxValue?: any, length?: any, isArray: boolean = false) {
+  constructor(propertyType: string, options: ISpecPropertyOptions = {}) {
 
     this.PropertyType = propertyType
-    this.Nullable = nullable
-    this.AllowedValues = allowedValues
-    this.DefaultValue = defaultValue
-    this.MinValue = minValue
-    this.MaxValue = maxValue
-    this.PrimaryKey = primaryKey
-    this.Unique = unique
-    this.DeleteFlag = deleteFlag
-    this.Length = length
-    this.IsArray = isArray
-    if (Array.isArray(autogenerateOptions)) {
-      if (autogenerateOptions.length !== 2) {
-        throw new RangeError(`Expected 2 elements in autogenerate options, received ${autogenerateOptions}`)
+    this.Nullable = options.nullable || false
+    this.AllowedValues = options.allowedValues
+    this.DefaultValue = options.defaultValue
+    this.MinValue = options.minValue
+    this.MaxValue = options.maxValue
+    this.PrimaryKey = options.primaryKey || false
+    this.Unique = options.unique || false
+    this.DeleteFlag = options.deleteFlag || false
+    this.Length = options.length
+    this.IsArray = options.isArray || false
+    this.Precision = options.precision
+    this.Scale = options.scale
+    if (Array.isArray(options.autogenerateOptions)) {
+      if (options.autogenerateOptions.length !== 2) {
+        throw new RangeError(`Expected 2 elements in autogenerate options, received ${options.autogenerateOptions}`)
       } else {
-        this.AutogenerateOptions = new IntegerAutogenerateOptions(autogenerateOptions[0], autogenerateOptions[1])
+        this.AutogenerateOptions = new IntegerAutogenerateOptions(options.autogenerateOptions[0], options.autogenerateOptions[1])
       }
     } else {
-      this.AutogenerateOptions = autogenerateOptions
+      this.AutogenerateOptions = options.autogenerateOptions
     }
   }
 }
