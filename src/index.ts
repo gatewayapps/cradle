305c7652254363b8ff3d90e726f7bbabde9581cc
Daniel Gary <daniel@gatewayapps.com>
2018-10-23 13:05:14 -0500

    exported ICradleOperation (#32)

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export {default as  CradleModel} from './lib/CradleModel'
 export {default as  CradleSchema} from './lib/CradleSchema'
 export {default as  EmitterOptions} from './lib/EmitterOptions'
 
+export {ICradleOperation} from './lib/ICradleOperation'
 export {default as  ICradleEmitter} from './lib/ICradleEmitter'
 export {default as  ICradleLoader } from './lib/ICradleLoader'
 export {default as  LoaderOptions} from './lib/LoaderOptions'
