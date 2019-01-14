ff7037dfc875a3ee2c8fecbac8a7c4870efc2569
Daniel Gary <daniel@gatewayapps.com>
2019-01-14 12:50:39 -0600

    default to ./cradle.config.js if not specified

--- src/cli/utils/config.ts
+++ src/cli/utils/config.ts
@@ -3,10 +3,7 @@ import path from 'path'
 import CradleConfig from '../../lib/CradleConfig'
 
 export function loadConfiguration(configFile: string): CradleConfig {
-  if (!configFile) {
-    throw new Error('Cradle config file is required')
-  }
-
+  configFile = configFile || './cradle.config.js'
   const finalPath = path.join(process.cwd(), configFile)
   if (fs.existsSync(finalPath)) {
     const cradleConfig = require(finalPath)
