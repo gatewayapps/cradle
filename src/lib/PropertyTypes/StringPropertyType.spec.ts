e1852cfbc867c46ab7d23959422fc9814e4e728a
Daniel Gary <daniel@gatewayapps.com>
2019-06-10 21:35:16 -0500

    fixed issues with tests

--- src/lib/PropertyTypes/StringPropertyType.spec.ts
+++ src/lib/PropertyTypes/StringPropertyType.spec.ts
@@ -1,12 +1,12 @@
 import { expect } from 'chai'
 import 'mocha'
-import constants from './constants'
-import StringPropertyType from './StringPropertyType'
+import { PropertyTypes } from './constants'
+import { StringPropertyType } from './StringPropertyType'
 
 describe('StringPropertyType', () => {
   describe('constructor', () => {
     it('Should return a PropertyType with a type name of String', () => {
-      expect(new StringPropertyType({}).TypeName).to.equal(constants.String)
+      expect(new StringPropertyType({}).TypeName).to.equal(PropertyTypes.String)
     })
     it('Should correctly assign unique as false', () => {
       expect(new StringPropertyType({ Unique: false, AllowNull: false, IsPrimaryKey: false })).to.deep.include({ Unique: false })
