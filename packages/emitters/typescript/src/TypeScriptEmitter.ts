57cfc51e01105dc125ccc69e433bf1d56321057c
Daniel Gary <daniel@gatewayapps.com>
2019-06-25 17:33:28 -0500

    incremented versions

--- packages/emitters/typescript/src/TypeScriptEmitter.ts
+++ packages/emitters/typescript/src/TypeScriptEmitter.ts
@@ -138,9 +138,9 @@ export class TypeScriptEmitter extends FileEmitter {
         const referenceType = propertyType as ReferenceModelType
         return `I${referenceType.ModelName}`
       }
-      case PropertyTypes.Object: {
-        return 'object'
-      }
+      // case PropertyTypes.Object: {
+      //   return 'object'
+      // }
       default: {
         return 'any'
       }
